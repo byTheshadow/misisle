4fb30ca74a6956c934c6b120ec039c828bbdcd15
Shadow <loud23for@gmail.com>
2026-07-26 18:07:43 +0800

    更新

--- src/lib/stores/zicard.ts
+++ src/lib/stores/zicard.ts
@@ -256,10 +256,14 @@ export const useZicardStore = create<ZicardState>((set, get) => ({
   createSessionFromCharacter: async (character) => {
     const now = Date.now()
 
-    let globalLibrary = await db.zicardLibraries
-      .where('characterId')
-      .equals(null as any)
-      .first()
+    let globalLibrary = (
+  await db.zicardLibraries.toArray()
+).find(
+  (library) =>
+    library.characterId === null &&
+    library.scope === 'global'
+)
+
 
     if (!globalLibrary) {
       globalLibrary = {
